3935a6177c1296ed7f2240c87c8b2d3e19c5077a
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2021-12-06 23:10:05 -0600

    Completed rock paper scissors program

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,60 @@
 
-
-
-
-
-
-
-
-
+// Randomly generate a play for the computer
 function computerPlay() {
-    let rock = "Rock",
-    scissors = "Scissors",
-    paper = "Paper"
+    let plays = ["rock", "paper", "scissors"];
 
-    return random;
-
+    return plays[Math.floor(Math.random() * plays.length)];
 }
+
+// Play a round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+
+    if (computerSelection == playerSelection) {
+        console.log("It's a tie! You chose " + playerSelection + " and the computer chose " + computerSelection + "!!");
+        return ties += 1;
+    }
+
+    if ((playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "scissors" && computerSelection == "rock") ||
+        (playerSelection == "paper" && computerSelection == "scissors")) {
+            console.log("You lost! " + computerSelection + " beats " + playerSelection + "!!");
+        return losses +=1;
+        }
+
+    else {
+        console.log("You won! " + playerSelection + " beats " + computerSelection + "!!");
+        return wins +=1;
+    }
+}
+
+
+
+// Loop for a total of 5 rounds 
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+
+        let computerSelection = computerPlay();
+        let playerSelection = prompt("Make a selection: 'Paper', 'Rock', or 'Scissors'").toLowerCase();
+        playRound(playerSelection,computerSelection);
+    }
+}
+
+function declareWinner() {
+    
+    if (wins > losses) {
+        console.log("You win!!");
+    }
+    if (losses > wins) {
+        console.log("You suck and are a loser!!");
+    }
+    if (wins == losses) {
+        console.log("The game is tied, you didn't lose, but you didn't win either!");
+    }
+}
+
+let wins = 0;
+let losses = 0;
+let ties = 0;
+game();
+declareWinner();
